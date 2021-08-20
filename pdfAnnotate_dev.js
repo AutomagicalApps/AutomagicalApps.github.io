af60f739ff2e14c7b0daf2fb7e0a148e29eea2bd
AutomagicalApps <55783564+AutomagicalApps@users.noreply.github.com>
2021-08-20 07:33:59 -1000

    Update pdfAnnotate_dev.js
    Changed new Rectangle in fabric init to new Shape...do we need this declaration? Why var arrow?

--- pdfAnnotate_dev.js
+++ pdfAnnotate_dev.js
@@ -88,7 +88,7 @@ var PDFAnnotate = function(container_id, url, selectBool, options = {}) {
 	                color: inst.color
 	            }
 	        });
-		var arrow = new Rectangle(fabricObj);
+		var arrow = new Shape(fabricObj);
 			inst.fabricObjects.push(fabricObj);
 			if (typeof options.onPageUpdated == 'function') {
 				fabricObj.on('object:added', function() {
